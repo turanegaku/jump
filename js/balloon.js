433f7808e9d90b739d39dd9c609b9cac8b1a3003
turanegaku <turanegaku@gmail.com>
2016-03-26 17:19:15 +0900

    baloon game

--- js/balloon.js
+++ js/balloon.js
@@ -1,5 +1,4 @@
 var score = 0;
-var hiscore = 99.99;
 var combo = 0;
 
 var start_time;
@@ -14,6 +13,12 @@ var shoot_frame = 20;
 
 var add_frame = 0;
 
+var apple_img;
+var balloon_img = [];
+
+var result_score;
+var result_frame = 0;
+
 function Balloon(x, y, apple, diff) {
   this.x = x;
   this.y = y;
@@ -21,6 +26,7 @@ function Balloon(x, y, apple, diff) {
   this.dx = 0;
   this.ddy = 0;
   this.r = 50;
+  this.i = int(random(3));
 
   if (apple) {
     this.r = 20;
@@ -30,9 +36,11 @@ function Balloon(x, y, apple, diff) {
   }
 
   this.draw = function() {
-    fill(255, 100, 100);
-    stroke(255, 200, 200);
-    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    if (apple) {
+      image(apple_img, this.x, this.y, this.r * 3, this.r * 3);
+    } else {
+      image(balloon_img[this.i], this.x, this.y + 18, this.r * 6, this.r * 6);
+    }
   };
 
   this.update = function() {
@@ -55,7 +63,6 @@ var balloons = [];
 
 var TITLE = 0;
 var GAME = 1;
-var RESULT = 1;
 var step = TITLE;
 
 function setup() {
@@ -63,6 +70,13 @@ function setup() {
 
   mx = width / 2;
   my = height / 2;
+
+  apple_img = loadImage('./img/apple.png');
+  balloon_img.push(loadImage('./img/balloon1.png'));
+  balloon_img.push(loadImage('./img/balloon2.png'));
+  balloon_img.push(loadImage('./img/balloon3.png'));
+
+  imageMode(CENTER);
 }
 
 function draw() {
@@ -74,14 +88,13 @@ function draw() {
   if (shoot_time > 0) {
     shoot_time--;
   }
-  if (step == GAME) {
-    $.each(balloons, function(i, v) {
-      v.update();
-    });
-    $.each(balloons, function(i, v) {
-      v.draw();
-    });
-  } else if (step == TITLE) {
+  $.each(balloons, function(i, v) {
+    v.update();
+  });
+  $.each(balloons, function(i, v) {
+    v.draw();
+  });
+  if (step == TITLE) {
     textSize(20);
     textAlign(CENTER);
     noStroke();
@@ -89,7 +102,16 @@ function draw() {
     ellipse(width / 2, height / 2, 100, 100);
     stroke(0);
     fill(255);
+    noStroke();
     text("START!!", width / 2, height / 2 + 10);
+
+    if (result_frame > 0) {
+      result_frame--;
+      var dd = result_frame - 50;
+      fill(0);
+      textSize(30);
+      text(result_score.format('mm:ss.SS'), width / 2 + max(0, dd * dd - 500), height / 4);
+    }
   }
   var p = sq(shoot_time / shoot_frame);
   var r = 20 * (1 - p);
@@ -98,17 +120,20 @@ function draw() {
   line(mx + r * cos(c), my + r * sin(c), mx + r * cos(c + PI), my + r * sin(c + PI));
   c += HALF_PI;
   line(mx + r * cos(c), my + r * sin(c), mx + r * cos(c + PI), my + r * sin(c + PI));
-  if (step == GAME && combo > 0) {
-    textSize(30);
-    textAlign(CENTER);
-    fill(0);
-    text(combo + ' CHAIN', width / 2, 60);
+  if (step == GAME) {
+    if (combo > 0) {
+      textSize(30);
+      textAlign(CENTER);
+      fill(0);
+      text(combo + ' CHAIN', width / 2, 60);
+    }
+
+    if (--add_frame <= 0) {
+      add_frame = map(combo, 0, 30, 100, 10);
+      balloons.push(new Balloon(random(100, width - 100), height + 100, false, map(score, 0, 10000, 1, 3)));
+    }
   }
 
-  if (--add_frame <= 0) {
-    add_frame = map(combo, 0, 50, 100, 10);
-    balloons.push(new Balloon(random(100, width - 100), height + 100, false, map(score, 0, 10000, 1, 3)));
-  }
   balloons = balloons.filter(function(v) {
     if (v.x > -200 && v.y > -200 && v.x < width + 200 && v.y < height + 200)
       return true;
@@ -143,8 +168,12 @@ function shoot() {
   }
 
   if (score >= 10000) {
+    score = 10000;
     step = TITLE;
-    hims = ms;
+    result_score = ms;
+    result_frame = 100;
+    if (ms < hims)
+      hims = ms;
   }
 }
 
@@ -158,7 +187,9 @@ function mousePressed() {
     if ((mx - width / 2) * (mx - width / 2) + (my - height / 2) * (my - height / 2) < 50 * 50) {
       step = GAME;
       start_time = moment();
+      score = 0;
       frameCount = 0;
+      combo = 0;
       balloons = [];
     }
     return;
